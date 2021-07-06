79070d13a774ae8949791513950cbc44da2bf441
shadab0799 <shadab0799@gmail.com>
2021-07-07 01:49:52 +0530

    adding database-2

--- ds/settings.py
+++ ds/settings.py
@@ -22,8 +22,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-#SECRET_KEY = os.environ['SECRET_KEY']
-SECRET_KEY = 't(=k0!1rhmw8zc8&v0co(u)tgigwow3)s=x$1d70=+gcy#i-+%'
+SECRET_KEY = os.environ['SECRET_KEY']
+#SECRET_KEY = 't(=k0!1rhmw8zc8&v0co(u)tgigwow3)s=x$1d70=+gcy#i-+%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
